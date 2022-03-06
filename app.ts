fe3f2199059a667f1744795d073798671f51cb2b
Lior Grishin <94124882+lorGR@users.noreply.github.com>
2022-03-06 22:57:04 +0200

    Day 1 Version 2

--- app.ts
+++ app.ts
@@ -1,16 +1,13 @@
 const graphBox:HTMLDivElement = document.querySelector('#graphBox')
 
-const arr:Array<number> = [60,40,30]
+const arr:Array<number> = [60,40,10,35]
 
 function createColums(arr:Array<number>){
     arr.forEach(element => {
         const newColum:HTMLDivElement = document.createElement('div')
         console.log(element)
+        newColum.classList.add('graph')
         newColum.style.height = `${element}%`
-        newColum.style.width = `30px`
-        newColum.style.backgroundColor = 'teal'
-        newColum.style.position = 'absolute'
-        newColum.style.bottom = `0`
         graphBox.appendChild(newColum)
     })
 }
--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,11 @@
 var graphBox = document.querySelector('#graphBox');
-var arr = [60, 40, 30];
+var arr = [60, 40, 10, 35];
 function createColums(arr) {
     arr.forEach(function (element) {
         var newColum = document.createElement('div');
         console.log(element);
+        newColum.classList.add('graph');
         newColum.style.height = element + "%";
-        newColum.style.width = "30px";
-        newColum.style.backgroundColor = 'teal';
-        newColum.style.position = 'absolute';
-        newColum.style.bottom = "0";
         graphBox.appendChild(newColum);
     });
 }
