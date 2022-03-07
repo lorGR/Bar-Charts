a6b8d97a50a0c017f23cd8799be746cb7fae32fd
Lior Grishin <94124882+lorGR@users.noreply.github.com>
2022-03-07 19:20:15 +0200

    CreateColumn is working

--- app.ts
+++ app.ts
@@ -1,13 +1,18 @@
 const graphBox:HTMLDivElement = document.querySelector('#graphBox')
 
-const arr:Array<number> = [60,40,10,35]
+const arr:Array<number> = [60,40]
 
 function createColums(arr:Array<number>){
     arr.forEach(element => {
         const newColum:HTMLDivElement = document.createElement('div')
+        const newText = document.createElement('span')
         console.log(element)
         newColum.classList.add('graph')
         newColum.style.height = `${element}%`
+        newText.classList.add('textPrecentage')
+        newText.innerHTML = `${element}%`
         graphBox.appendChild(newColum)
+        newColum.appendChild(newText)
     })
+        
 }
--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,15 @@
 var graphBox = document.querySelector('#graphBox');
-var arr = [60, 40, 10, 35];
+var arr = [60, 40];
 function createColums(arr) {
     arr.forEach(function (element) {
         var newColum = document.createElement('div');
+        var newText = document.createElement('span');
         console.log(element);
         newColum.classList.add('graph');
         newColum.style.height = element + "%";
+        newText.classList.add('textPrecentage');
+        newText.innerHTML = element + "%";
         graphBox.appendChild(newColum);
+        newColum.appendChild(newText);
     });
 }
